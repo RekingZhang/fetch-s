deec42a4ec1b0d352669b25e2a90216b86c3763e
zhangkaiqiang <zhangkaiqiang@58ganji.com>
2018-04-11 17:06:36 +0800

    test(instance): add test to create instance

---
+++ test/unit/create.spec.js
@@ -0,0 +1,38 @@
+import fetchMock from 'fetch-mock';
+import fetchs from './../../src/index';
+
+describe('创建实例单元测试', () => {
+	beforeEach(function() {
+		const headers = {
+			'Content-Type': 'application/json;charset=utf-8',
+			'Access-Control-Allow-Origin': '*'
+		};
+		const body = { foo: 'bar' };
+
+		fetchMock.get('http://www.example.com/api/', {
+			status: 200,
+			body,
+			headers
+		});
+	});
+
+	afterEach(function() {
+		fetchMock.restore();
+	});
+	it('json', async () => {
+		const instance = fetchs.create({
+			origin: 'https://www.example.com'
+		});
+
+		await instance
+			.get('/api/', {
+				timeout: 1000
+			})
+			.then(r => {
+				expect(r.data).toEqual({ foo: 'bar' });
+			})
+			.catch(e => {
+				console.dir('error', e);
+			});
+	});
+});
