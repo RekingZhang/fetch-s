51d3a58416aaeeaa2b71ded2e216b9d31fe4f961
zhangkaiqiang <zhangkaiqiang@58.com>
2018-11-26 11:34:12 +0800

    build: output the result of the build

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,19 @@ const CleanWebpackPlugin = require('clean-webpack-plugin'); //每次构建清理
 module.exports = {
 	entry: './index.js',
 	output: {
-		filename: 'fetch-s.min.js',
+		filename: 'fetchs.min.js',
 		library: 'fetchs',
 		libraryTarget: 'umd',
 		path: path.resolve(__dirname, 'dist')
 	},
 	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader'
 			}
-		]
+		}]
 	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),
